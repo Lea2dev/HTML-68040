f5a25c974699913bc644c66887d861dbd12aba3a
Leandro <leandro.pais@educacionit.com>
2023-09-01 00:40:05 -0300

    fin clase 7

--- InterfazWeb/php/contacto.php
+++ InterfazWeb/php/contacto.php
@@ -1,17 +1,17 @@
 <?php
-    print_r($_POST);
-    echo '<br><br>';
+print_r($_POST);
+echo '<br><br>';
 
-    $to = "leandrodpais@gmail.com";
-    $subject = "Mail desde el formulario";
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers = $headers . "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From: webmaster@example.com' . "\r\n" .
-    //    'Reply-To: webmaster@example.com' . "\r\n";
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $comentarios = $_POST['comentarios'];
-    $message = "
+$to = "leandrodpais@gmail.com";
+$subject = "Mail desde el formulario";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers = $headers . "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: 	webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n";  // aca debieran ir direcciones válidas del dominio que registramos
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$comentarios = $_POST['comentarios'];
+$message = "
             <html>
                 <head>
                    <title>HTML</title>
@@ -23,10 +23,10 @@
                     <p>Comentarios: $comentarios </p> 
                 </body>
                 </html>";
-    
-    mail($to, $subject, $message, $headers);
-                
-    echo '<br><br><h1>
+
+mail($to, $subject, $message, $headers);
+
+echo '<br><br><h1>
             <center>Gracias por comunicarse con nosotros</center>
             </h1>';
 
