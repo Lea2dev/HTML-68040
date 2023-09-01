42fe398257c0ffb7068db8424fb49336e58ec60d
Leandro <leandro.pais@educacionit.com>
2023-08-31 21:59:09 -0300

    Fin clase

--- Modulo4/ejemplo.php
+++ Modulo4/ejemplo.php
@@ -9,10 +9,11 @@ $nombre = "Leandro";
 echo($nombre);
 
 echo ("<br>");
-$datosUsuario = ["Leandro", "Pais", "leandro@mail.com"];
+$datosUsuario = [ "Leandro", "Pais", "leandro@mail.com"];
 //                   0         1             2
 
 echo ($datosUsuario[1]);
-
+echo("<br>");
+print_r($datosUsuario);
 
 ?>
