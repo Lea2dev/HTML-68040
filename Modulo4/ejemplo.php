3567e9c2c82a078e877bbc499012a7e3664f1b76
Leandro <leandro.pais@educacionit.com>
2023-08-31 21:38:31 -0300

    Modulo 4

---
+++ Modulo4/ejemplo.php
@@ -0,0 +1,18 @@
+<?php
+
+// hola
+
+echo("Hola");
+echo("<br><h2>Hola como titulo</h2>");
+
+$nombre = "Leandro";
+echo($nombre);
+
+echo ("<br>");
+$datosUsuario = ["Leandro", "Pais", "leandro@mail.com"];
+//                   0         1             2
+
+echo ($datosUsuario[1]);
+
+
+?>
